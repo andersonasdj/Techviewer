648c7d26c8b9b0c742af55cc28ca5ef839673b16
Anderson de Araujo <andersonasdj@gmail.com>
2023-04-05 11:44:50 -0300

    Projeto de módulo de visão sistema de chamados

---
+++ Techviewer/src/main/java/br/com/techgold/TechviewerApplication.java
@@ -0,0 +1,13 @@
+package br.com.techgold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechviewerApplication.class, args);
+	}
+
+}
